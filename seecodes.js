3fad16bd67cf1070f7081e389be0f7bed243ccc0
seeyin <30359058+seeyin@users.noreply.github.com>
2017-07-24 21:48:46 +0800

    seelearnstocode
    I learn to code

---
+++ seecodes.js
@@ -0,0 +1,13 @@
+function Awesome() {
+    alert('Thanks! I love you! :)');
+}
+
+function Seriously() {
+    alert('Oh why :(');
+}
+
+function thumbsup() {
+    window.location = 'https://www.facebook.com/seeyintan';
+    target="_blank";
+}
+
